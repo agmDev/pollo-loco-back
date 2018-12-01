b7bf9867f2d59ce6841dd86f92d498a5a572a4c9
agm <agmd.moe@gmail.com>
2018-12-01 23:28:48 +0100

    2 connect

--- controler/requestHandler.js
+++ controler/requestHandler.js
@@ -1,31 +1,4 @@
-const Sequelize = require('sequelize');
-const config = require('../config/database');
 const { createUser, getUser } = require('../db/models/user.js');
-
-const sequelize = new Sequelize(
-  config.development.database,
-  config.development.username,
-  config.development.password, {
-    host: config.development.host,
-    dialect: config.development.dialect,
-    port: config.development.port,
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000,
-    },
-  },
-);
-
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully.');
-  })
-  .catch((err) => {
-    console.error('Unable to connect to the database:', err);
-  });
 
 const subscriptionPost = async (req, res) => {
   const {
